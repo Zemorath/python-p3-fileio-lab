b957fc0b5eebe40c8a8692141812aa1d86985d50
Zemorath <trentward10@gmail.com>
2023-12-07 20:42:59 -0600

    Fileio Lab Complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,13 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode="w") as f:
+        f.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    with open(f'{file_name}.txt', mode="a") as f:
+        f.write(append_content)
 
 def read_file(file_name):
-    pass
+    # text_file = open(f'{file_name}.txt', mode="r")
+    # print(text_file.read())
+    with open(f'{file_name}.txt', mode="r") as f:
+        return f.read()
